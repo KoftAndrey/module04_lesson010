0e7adfbd8a1f53bbcfc54263044e2990cb40febd
Andrey <KoftAndrey@gmail.com>
2023-03-03 12:29:57 +0300

    first commit

---
+++ cart.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const cart = {
+  items: [],
+  count: 0,
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+  add(productName, productPrice, productQuantity = 1) {
+    this.items.push({
+      name: productName,
+      price: productPrice,
+      quantity: productQuantity,
+    });
+    this.increaseCount(productQuantity);
+  },
+  increaseCount(number) {
+    this.count += number;
+  },
+  calculateItemPrice() {
+    return this.items.reduce((sum, item) => {
+      sum += item.price * item.quantity;
+      return sum;
+    }, 0);
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(this.totalPrice);
+  },
+};
+
+cart.add('Яблоко', 10, 5);
+cart.add('Апельсин', 15, 4);
+cart.add('Арбуз', 100, 2);
+cart.print();
